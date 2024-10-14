0c61f22a3a2b0f7b1c7325cb3b22f30d45c85cd3
ortiz.leal.roger <ortiz.leal.roger@alumnat.copernic.cat>
2024-10-14 17:08:12 +0200

    Afegeix script per fer cleanup de Vagrant

---
+++ ddbb/reset.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal
+
+set "FOLDER=C:\Users\%USERNAME%\VirtualBox VMs"
+
+for /d %%i in ("%FOLDER%\*subasterra*") do (
+    echo Borrant carpeta: %%i
+    rd /s /q "%%i"
+)
+
+for /d %%i in ("%FOLDER%\*ddbb*") do (
+    echo Borrant carpeta: %%i
+    rd /s /q "%%i"
+)
+
+for /d %%i in (".\.vagrant") do (
+    echo Borrant carpeta: %%i
+    rd /s /q "%%i"
+)
+
+for %%i in (".\*.log") do (
+    echo Borrant fitxer: %%i
+    del /q "%%i"
+)
+
+echo Cleanup completat.
+endlocal
