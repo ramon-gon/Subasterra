027f8edebe55bb7ac7741b62bec24c30416c01df
ortiz.leal.roger <ortiz.leal.roger@alumnat.copernic.cat>
2024-11-07 16:45:52 +0100

    Fixes disseny

--- scripts/modal.js
+++ scripts/modal.js
@@ -23,3 +23,30 @@ document.querySelectorAll('.assign-btn').forEach(button => {
       };
   });
 });
+
+document.querySelectorAll('.assign-btn-dropdown').forEach(button => {
+    button.addEventListener('click', event => {
+        event.preventDefault();
+        const productId = button.getAttribute("data-product-id");
+        const modal = document.getElementById(`auction-modal-confirm_${productId}`);
+        const modalClose = document.getElementById(`close-modal_${productId}`);
+        const denyAssignButton = modal.querySelector(`#not-assign`);
+  
+        modal.style.display = "block";
+  
+        modalClose.onclick = function() {
+            modal.style.display = "none";
+        };
+  
+        denyAssignButton.onclick = function () {
+            modal.style.display = "none";
+        };
+  
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        };
+    });
+  });
+  
